30528eb789c8773918ac53d979502c7969b11264
ticet11 <BriKozub@gmail.com>
2020-04-16 13:20:58 -0600

    Overview of Ranges in Python Lists

--- data-types/lists-dictionaries-tuples.py
+++ data-types/lists-dictionaries-tuples.py
@@ -35,7 +35,7 @@ users = ['Jan', 'Brian', 'Beth', 'Brian', 'Brian']
 
 print(users)
 
-## Will remove first instance
+## Will remove first instance (Have to know the value of the element to remove)
 users.remove('Brian')
 
 print(users)
@@ -46,8 +46,115 @@ popped_user = users.pop()
 print(users)
 print(popped_user)
 
+## simply deletes the chosen element from list (Have to know the index of the element to remove)
 del users[2]
 print(users)
+
+########################################
+# Nested Lists and Multiple Data Types #
+########################################
+
+users = ['Brian', 'Dand', 'Joel']
+
+ids = [1,2,3]
+
+# Mixed lists can be dangerous. If you try to do an operation that only works on ints on this list, like add,
+# you will have an error
+mixed_list = [42, 10.3, 'Altuve', users]
+
+print(mixed_list)
+
+user_list = mixed_list.pop()
+
+print(user_list)
+print(mixed_list)
+
+# Everything in this array is the same, because they are all arrays
+nested_list = [[123], [234], [345], ['Brian']]
+
+#########################################################
+# Query Processes: len(), Negative Indexes, and index() #
+#########################################################
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+number_of_tags = len(tags)
+last_item = tags[-1]
+index_of_last_item = tags.index(last_item)
+
+print(number_of_tags)
+print(last_item)
+print(index_of_last_item)
+
+#################
+# Sorting Lists #
+#################
+
+## Organizes in alphabetical order or numerical order
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort()
+
+print(tags)
+
+tags.sort(reverse=True)
+
+print(tags)
+
+totals = [234, 1, 543, 2345]
+
+totals.sort(reverse=True)
+
+print(totals)
+
+###########################################
+# Join Function to build URL Query String #
+###########################################
+
+# https://www.google.com/search?q=python+development+tutorial
+
+uri = "https://www.google.com/search?q="
+# tags = ['python', 'development', 'tutorial']
+
+# # input requests an input from the user
+# user_input = input("Name your pleasure!")
+# tags = user_input.split()
+
+
+# puts the string in between each of the elements
+formatted_tags = '+'.join(tags)
+query_uri = f'{uri}{formatted_tags}'
+
+print(query_uri)
+
+####################
+# Ranges in a List #
+####################
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+# pretty much the same as string ranges
+tag_range = tags[1:2]
+print(tag_range)
+
+# This will grab everything from slice 1 to the end
+tag_range = tags[1:]
+print(tag_range)
+
+# Grab everything from the beginning to slice 2.
+tag_range = tags[:2]
+print(tag_range)
+
+# Grab all but the last element
+tag_range = tags[:-1]
+print(tag_range)
+
+# Grab everything, for some reason
+tag_range = tags[:]
+print(tag_range)
 
 # # Dictionary
 # car_dictionary = {
