e1584e220f7786c17ea7871c7507aae6ea2d97f8
ticet11 <BriKozub@gmail.com>
2020-04-16 16:45:12 -0600

    How to Add New Key/Value Pairs to Python Dictionaries

--- data-types/dictionaries.py
+++ data-types/dictionaries.py
@@ -14,7 +14,33 @@ sensible_car = car_dictionary["sensible"]
 
 print(sensible_car)
 
-## will throw an error. Can not use a key that isn't defined.
+## will throw an error. Can not use a key that isn't defined or index.
 # print(car_dictionary[butt])
+# print(car_dictionary[0])
 
-print(car_dictionary[0])
+######################
+# Nested Collections #
+######################
+
+teams = {
+    "Sabres": ["Miller", "Roy", "Grier"],
+    "Ducks" : ["Trout", "Puhols"],
+    "Avalanche": ["Judge", "Stanton"]
+}
+
+print(teams["Sabres"])
+print(teams["Ducks"])
+print(teams["Avalanche"])
+
+# setting a variable as a key/value pair, from the dictionary
+sabres = teams["Sabres"]
+
+print(sabres)
+
+#######################
+# Add Key/Value Pairs #
+#######################
+
+teams['Lightening'] = ["Price", 'Betts']
+
+print(teams)
