d97649959d88bfb1b3be550408c3727bb0e447d0
ticet11 <BriKozub@gmail.com>
2020-04-17 16:33:15 -0600

    How to Implement Python Loops for Lists, Tuples, and Dictionaries

---
+++ loops/basics.py
@@ -0,0 +1,16 @@
+players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+players = ('Altuve', 'Bregman', 'Correa', 'Gattis')
+
+for player in players:
+  print(player)
+
+players = {
+  '2b': 'Altuve',
+  '3b': 'Bregman',
+  'ss': 'Correa',
+  'dh': 'Gattis'
+}
+
+for position, player in players.items():
+  print('Position', position)
+  print('Player', player)
