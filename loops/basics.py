dfa570dbd61027c08262dba9e8538d330bbd3313
ticet11 <BriKozub@gmail.com>
2020-04-17 17:10:05 -0600

    Guide to Continue and Break in Python Loops

--- loops/basics.py
+++ loops/basics.py
@@ -26,3 +26,38 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 for letter in alphabet:
   print(letter)
+
+#######################
+# Loop through ranges #
+#######################
+
+for num in range(1, 10):
+  print(num)
+
+for num in range(1, 10, 2):
+  print(num)
+
+######################
+# Continue and Break #
+######################
+
+usernames = [
+  'jon',
+  'tyrion',
+  'theon',
+  'cersei',
+  'sansa',
+]
+
+for username in usernames:
+  if username == 'cersei':
+    print(f'Sorry, {username}, you are not allowed')
+    continue
+  else:
+    print(f'{username} is allowed')
+
+for username in usernames:
+  if username == 'cersei':
+    print(f'{username} was found at index {usernames.index(username)}')
+    break
+  print(username)
