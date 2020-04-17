f43b8b5959412010cadf454324762340a52de4d1
ticet11 <BriKozub@gmail.com>
2020-04-17 15:39:42 -0600

    Three Ways to Remove Elements from a Python Tuple

--- data-types/tuples.py
+++ data-types/tuples.py
@@ -50,3 +50,22 @@ print(post[-1][1])
 post = ('Python Basics', 'Intro guide to Python', 'Some cool python content')
 
 print(post[1::2])
+
+###################
+# Remove Elements #
+###################
+
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+# Removing elements from end
+post = post[:-1]
+
+# Removing elements from beginning
+post = post[1:]
+
+# Removing specific element (messy/not recommended)
+post = list(post)
+post.remove('published')
+post = tuple(post)
+
+print(post)
