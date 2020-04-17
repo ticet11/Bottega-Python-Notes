1e28a83d2e2fdeb05cb7cce62ab7e26111153146
ticet11 <BriKozub@gmail.com>
2020-04-17 16:06:28 -0600

    Introduction to the Python Set Data Structure

---
+++ data-types/sets.py
@@ -0,0 +1,19 @@
+# Uniqueness
+# Set is unordered
+tags = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+print(tags)
+
+# Nope
+# print(tags[0])
+
+query_one = 'python' in tags
+query_two = 'ruby' in tags
+
+print(query_one)
+print(query_two)
