e8086a3251bccd008624d71199e89e1270d58983
ticet11 <BriKozub@gmail.com>
2020-04-17 16:10:40 -0600

    Various Methods for Merging Python Sets

--- data-types/sets.py
+++ data-types/sets.py
@@ -1,4 +1,4 @@
-# Uniqueness
+# Uniqueness - no duplicates
 # Set is unordered
 tags = {
   'python',
@@ -17,3 +17,37 @@ query_two = 'ruby' in tags
 
 print(query_one)
 print(query_two)
+
+################ 
+# Merging Sets #
+################
+
+tags_one = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+tags_two = {
+  'ruby',
+  'coding',
+  'tutorials',
+  'development'
+}
+
+# merged tags - removes duplicates
+merged_tags = tags_one | tags_two
+print(merged_tags)
+
+# tags in tags_one but not in tags_two
+exclusive_to_tag_one = tags_one - tags_two
+print(exclusive_to_tag_one)
+
+# tags in tags_two but not in tags_one
+exclusive_to_tag_two =  tags_two - tags_one
+print(exclusive_to_tag_two)
+
+# tags found in both tags_one and tags_two
+universal_tags = tags_one & tags_two
+print(universal_tags)
