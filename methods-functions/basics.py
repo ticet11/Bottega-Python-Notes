065db00c03e03751e97853d1e666f55416b28a0a
ticet11 <BriKozub@gmail.com>
2020-04-20 16:03:33 -0600

    Guide to Default Arguments in Python Functions

--- methods-functions/basics.py
+++ methods-functions/basics.py
@@ -63,3 +63,23 @@ def greeting(first, last):
 
 
 greeting('Kristine', 'Hudgens')
+
+#####################
+# Default Arguments #
+#####################
+
+def greeting(name = 'Guest'):
+  print(f'Hi {name}!')
+
+greeting()
+greeting('Kristine')
+
+# Nope
+def some_function(collection = []):
+  collection.append(1)
+  print(id(collection))
+  return collection
+
+
+print(some_function())
+print(some_function())
