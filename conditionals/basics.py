0644f03696c49f0e651778c1d8d55e00b6923db9
ticet11 <BriKozub@gmail.com>
2020-04-20 14:42:24 -0600

    How to Build Compound Conditionals in Python

--- conditionals/basics.py
+++ conditionals/basics.py
@@ -92,3 +92,37 @@ if 3 in nums:
   print('The number was found')
 else:
   print('The number was not found')
+
+#########################
+# Compound Conditionals #
+#########################
+
+username = 'jonsnow'
+email = 'jon@snow.com'
+password = 'thenorth'
+
+if username == 'jonsnow' and password == 'thenorth':
+  print('Access permitted')
+else:
+  print('Not allowed')
+
+
+if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+  print('Access permitted')
+else:
+  print('Not allowed')
+
+
+if username == 'jonsnow' or password == 'thenorth':
+  print('Access permitted')
+else:
+  print('Not allowed')
+
+
+logged_in = True
+standard_user = False
+
+if logged_in and not standard_user:
+  print('You can access the admin dashboard')
+else:
+  print('You can only access the standard dashboard')
