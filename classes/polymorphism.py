a1c3c9194450cd8c3c262f9b97ca1f059f0e5156
ticet11 <BriKozub@gmail.com>
2020-04-23 13:47:55 -0600

    Using Polymorphism to Build an HTML Generator in Python

---
+++ classes/polymorphism.py
@@ -0,0 +1,24 @@
+class Html:
+    def __init__(self, content):
+        self.content = content
+    
+    def render(self):
+        raise NotImplementedError('Subclass must implement render method.')
+
+class Heading(Html):
+    def render(self):
+        return f'<h1>{self.content}<h1>'
+
+class Div(Html):
+    def render(self):
+        return f'<div>{self.content}</div>'
+
+tags = [
+    Div('Some content'),
+    Heading('Some heading'),
+    Div('Another div')
+
+]
+
+for tag in tags:
+    print(tag.render())
