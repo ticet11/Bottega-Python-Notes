cfae766bc12fa9b5a72b78ccf18edccbcf9b1de9
ticet11 <BriKozub@gmail.com>
2020-04-23 10:18:20 -0600

    Guide to Python's __init__ Constructor Function

--- python_env/invoice.py
+++ python_env/invoice.py
@@ -8,3 +8,18 @@ print(inv_one.greeting())
 
 inv_two = Invoice()
 print(inv_two.greeting())
+
+class Invoice:
+
+    def __init__(self, client, total):
+        self.client = client
+        self.total = total
+    
+    def formatter(self):
+        return f'{self.client} owes: ${self.total}'
+
+google = Invoice('Google', 100)
+snapchat = Invoice('SnapChat', 200)
+
+print(google.formatter())
+print(snapchat.formatter())
