534ef26940e82fe683d47f0fcc1429cb2ffde869
ticet11 <BriKozub@gmail.com>
2020-04-23 11:13:52 -0600

    How to Work with Python Properties and Decorators

--- python_env/invoice.py
+++ python_env/invoice.py
@@ -1,15 +1,27 @@
 class Invoice:
 
     def __init__(self, client, total):
-        self.client = client
-        self.total = total
-    
+        self._client = client
+        self._total = total
+
     def formatter(self):
-        return f'{self.client} owes: ${self.total}'
+        return f'{self._client} owes: ${self._total}'
+
+    # Hints at the fact that you might want to access this information at some point
+    # Items not included are most likely things you shouldn't need to access
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, total):
+        self._total = total
+
 
 google = Invoice('Google', 100)
-print(google.client)
-
-google.client = 'Yahoo'
-
-print(google.client)
+google.total = 300
+print(google.formatter())
