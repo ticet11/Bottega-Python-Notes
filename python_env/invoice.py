df454b40cc8042e663b603d09d1f5afc3c0daa07
ticet11 <BriKozub@gmail.com>
2020-04-23 10:23:54 -0600

    How to Get and Set Data in a Python Class

--- python_env/invoice.py
+++ python_env/invoice.py
@@ -1,16 +1,5 @@
 class Invoice:
 
-    def greeting(self):
-        return('Hi there')
-
-inv_one = Invoice()
-print(inv_one.greeting())
-
-inv_two = Invoice()
-print(inv_two.greeting())
-
-class Invoice:
-
     def __init__(self, client, total):
         self.client = client
         self.total = total
@@ -19,7 +8,8 @@ class Invoice:
         return f'{self.client} owes: ${self.total}'
 
 google = Invoice('Google', 100)
-snapchat = Invoice('SnapChat', 200)
+print(google.client)
 
-print(google.formatter())
-print(snapchat.formatter())
+google.client = 'Yahoo'
+
+print(google.client)
