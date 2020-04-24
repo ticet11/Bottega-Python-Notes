2eecbb7055ae73977dd238dd3ee9b182680ce9e4
ticet11 <BriKozub@gmail.com>
2020-04-24 15:15:54 -0600

    How to Create a POST API Endpoint in Flask

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,14 +1,48 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-@app.route('/')
-def hello():
-    return 'Hey Flask'
+
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False)
+    content= db.Column(db.String(144), unique=False)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class GuideSchema(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
+
+# Endpoint to create a new guide.
+@app.route('/guide', methods=['POST'])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
 
 
 if __name__ == '__main__':
