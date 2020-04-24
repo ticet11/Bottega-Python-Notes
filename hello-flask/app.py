48a55fef7abb9aa812099db2b4639f04854a2434
ticet11 <BriKozub@gmail.com>
2020-04-24 13:51:50 -0600

    Added new dependencies to app.py

--- hello-flask/app.py
+++ hello-flask/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hey Flask'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
